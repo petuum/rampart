04e7076abc2c319fc663072b7de2f3e44647feab
Dian Li <dian.li@theiceberg.me>
2023-04-05 19:19:26 -0400

    Add license headers

--- tests/src/graph/test_utils.py
+++ tests/src/graph/test_utils.py
@@ -1,4 +1,17 @@
 # Copyright 2023 Petuum, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License."
+
 import pytest
 from src.graph.utils.tycheck import has_shared_prefix
 from src.graph.utils.classes import ValidationError, DeploymentError, collect_errors
